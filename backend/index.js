c8e64408101b13a8ba04c8cf8a510c88896f30b6
ubaid0309 <ubaidskms@gmail.com>
2024-03-10 16:15:04 +0530

    initialize app

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,11 @@
 const express = require("express");
+require('dotenv').config()
+
+const app = express();
+
+app.listen(process.env.PORT, (req, res) => {
+    console.log("Listening on port " + process.env.PORT);
+})
+
 
 
