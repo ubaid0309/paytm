cc0180f54edc5019de4ddbdad1317b8522f6178c
ubaid0309 <ubaidskms@gmail.com>
2024-03-15 23:17:33 +0530

    add : frontend routes skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import Dashboard from "./components/Dashboard";
+import Send from "./components/Send";
+
 function App() {
-  return <div className="">Hello world</div>;
+  return (
+    <div className="h-screen w-screen">
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send/:id" element={<Send />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
